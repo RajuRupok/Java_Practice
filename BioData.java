5b18d601a2ed83aab1272ec37694e68b5fe2f037
Raju Deb <rajurupok95@gmail.com>
2024-12-20 03:40:59 +0000

    Learned Escape Sequences

--- BioData.java
+++ BioData.java
@@ -2,6 +2,10 @@
 package com.mycompany.basicjava;
 public class BioData {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
+        System.out.println("Raju Deb\nBSc in CSE");
+        System.out.println("1 \t 2");
+        System.out.println("3 \t 4");
+        System.out.println("\"Java Programming\"");
+        
     }
 }
